15494adb6406d13887f009f4ad0fe755c7e8366e
Ivan Pepelnjak <ip@ipspace.net>
2021-05-26 09:20:33 +0200

    Move AF feature flag calculation into common module code

--- netsim/modules/eigrp.py
+++ netsim/modules/eigrp.py
@@ -8,12 +8,4 @@ from . import _Module
 class EIGRP(_Module):
 
   def node_post_transform(self, node: Box, topology: Box) -> None:
-    for af in ['ipv4','ipv6']:
-      if af in node.loopback:        # Address family enabled on loopback?
-        node.eigrp[af] = True         # ... we need it in IS-IS
-        continue
-
-      for l in node.get('links',[]): # Scan all links
-        if af in l:                  # Do we have AF enabled on any of them?
-          node.eigrp[af] = True       # Found it - we need it in IS-IS
-          continue
+    self.set_af_flag(node,node.eigrp)
--- netsim/modules/isis.py
+++ netsim/modules/isis.py
@@ -8,12 +8,4 @@ from . import _Module
 class ISIS(_Module):
 
   def node_post_transform(self, node: Box, topology: Box) -> None:
-    for af in ['ipv4','ipv6']:
-      if af in node.loopback:        # Address family enabled on loopback?
-        node.isis[af] = True         # ... we need it in IS-IS
-        continue
-
-      for l in node.get('links',[]): # Scan all links
-        if af in l:                  # Do we have AF enabled on any of them?
-          node.isis[af] = True       # Found it - we need it in IS-IS
-          continue
+    self.set_af_flag(node,node.isis)
